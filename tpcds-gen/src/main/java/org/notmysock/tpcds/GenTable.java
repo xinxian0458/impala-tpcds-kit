c3a9e5d85f033f985f8b69b5a52039e3c3fb3cf4
Jessie <jie.zhang10@dxc.com>
2019-09-04 11:23:16 +0800

    add some log

--- tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
+++ tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
@@ -192,12 +192,13 @@ public class GenTable extends Configured implements Tool {
             child = cmd[i+1];
           }
         }
-
+        System.err.println("Execute cmd: " + cmd);
         Process p = Runtime.getRuntime().exec(cmd, null, new File("dsdgen/tools/"));
         int status = p.waitFor();
         if(status != 0) {
           String err = readToString(p.getErrorStream());
-          throw new InterruptedException("Process failed with status code " + status + "\n" + err);
+          String input = readToString(p.getInputStream());
+          throw new InterruptedException("Process failed with status code " + status + "\n" + err + input);
         }
 
         File cwd = new File(".");
