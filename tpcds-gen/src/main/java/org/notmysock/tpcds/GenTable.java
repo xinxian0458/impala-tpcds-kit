4709161719835be52685becf1cc54574d3a1bae1
Jessie <jie.zhang10@dxc.com>
2019-09-05 09:20:12 +0800

    fixed log

--- tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
+++ tpcds-gen/src/main/java/org/notmysock/tpcds/GenTable.java
@@ -198,7 +198,7 @@ public class GenTable extends Configured implements Tool {
           String err = readToString(p.getErrorStream());
           String input = readToString(p.getInputStream());
           throw new InterruptedException("Process failed with status code " + status + "\n"
-        		  + "Execute cmd: " + cmd + "\n"
+        		  + "Execute cmd: " + command.toString() + "\n"
         		  + "Error " + err + "\n" 
         		  +	"Output " + input + "\n");
         }
